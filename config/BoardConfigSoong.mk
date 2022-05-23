eb98b97584d0022a9dc424910330107d078eee0a
jhenrique09 <jhsv09@gmail.com>
2022-05-23 21:18:20 +0000

    vendor: Allow using legacy fd
    Change-Id: I88e355e60de63a7f81adf8e25fabbd305fb0af7c
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -50,6 +50,7 @@ SOONG_CONFIG_atlantisGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_egl_display_array \
+    uses_legacy_fd_fbdev \
     uses_nothing_camera \
     uses_oplus_camera
 
@@ -83,6 +84,7 @@ SOONG_CONFIG_atlantisQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_
 SOONG_CONFIG_atlantisGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_atlantisGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_atlantisGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
+SOONG_CONFIG_atlantisGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDEV)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
